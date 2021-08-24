15fd835012b58a53e174fd22aedcbcc72de885ca
sarthak20shah <201701169@daiict.ac.in>
2021-08-24 17:40:10 +0530

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import { useSelector } from "react-redux";
+import "./App.css";
+import Counter from "./components/Counter";
 
 function App() {
+  const count = useSelector((state) => state.counter.count);
+  const voters = [
+    "Anthony Sistilli ",
+    "Bob Smith",
+    "Stephanie Foo",
+    "Kevin Ma",
+  ];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Redux made easy</h1>
+      <h2> Total Votes: {count}</h2>
+      {voters.map((voter) => (
+        <Counter name={voter} />
+      ))}
     </div>
   );
 }
